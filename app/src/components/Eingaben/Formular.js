ff3ade66d181ac07996c11f6ed43713b03adfbcc
simon-rdg <simon@stefan-rodig.de>
2022-05-07 12:33:28 +0200

    add streak validation

--- app/src/components/Eingaben/Formular.js
+++ app/src/components/Eingaben/Formular.js
@@ -27,6 +27,11 @@ class Formular extends React.Component {
     );
   }
 
+  renderCurrentDaySmall() {
+    const today = new Date();
+    return [today.getDate(), today.getMonth(), today.getFullYear()];
+  }
+
   renderError({ error, touched }) {
     if (touched && error) {
       return <div>Fehler</div>;
@@ -72,6 +77,149 @@ class Formular extends React.Component {
     );
   }
 
+  checkStreak(old) {
+    const today = new Date();
+
+    if (old[2] === today.getFullYear()) {
+      if (old[1] === today.getMonth()) {
+        if (old[0] === today.getDate()) {
+          // Dann handelt es sich um einen weiteren Eintrag am gleichen Tag
+          return 187;
+        } else if (old[0] === today.getDate() - 1) {
+          // Dann war die letzte Eingabe einen Tag zuvor;
+          // deckt aber nicht alle Möglichkeiten ab,
+          // wenn z.B. letzter Tag eines Monats auf den ersten des nächsten folgt.
+
+          return true;
+        }
+      } else if (old[1] === 3 || 5 || 8 || 10) {
+        // Dann war der letzte Eintrag in einem Monat mit 30 Tagen
+        // [April, Juni, September, November]
+
+        if (old[0] === 30) {
+          // Dann war die letzte Eingabe am letzten Tag des jeweiligen Monats
+          if (today.getDate() !== 1) {
+            // Dann ist die neue Eingabe nicht an einem ersten Tag im Monat
+            return false;
+          } else if (old[1] === 3) {
+            // Dann war die letzte Eingabe am 30. April des selben Jahres
+            if (today.getMonth() === 4) {
+              // Dann ist die neue Eingabe am 01. Mai und damit einen Tag später.
+              return true;
+            }
+          } else if (old[1] === 5) {
+            // Dann war die letzte Eingabe am 30. Juni des selben Jahres
+            if (today.getMonth() === 6) {
+              // Dann ist die neue Eingabe am 01. Juli und damit einen Tag später.
+              return true;
+            }
+          } else if (old[1] === 8) {
+            // Dann war die letzte Eingabe am 30. September des selben Jahres
+            if (today.getMonth() === 9) {
+              // Dann ist die neue Eingabe am 01. Oktober und damit einen Tag später.
+              return true;
+            }
+          } else if (old[1] === 10) {
+            // Dann war die letzte Eingabe am 30. November des selben Jahres
+            if (today.getMonth() === 11) {
+              // Dann ist die neue Eingabe am 01. Dezember und damit einen Tag später.
+              return true;
+            }
+          }
+        }
+      } else if (old[1] === 0 || 2 || 4 || 6 || 7 || 9) {
+        // Dann war der letzte Eintrag in einem Monat mit 31 Tagen
+        // [Januar, März, Mai, Juli, August, Oktober, Dezember]
+
+        if (old[0] === 31) {
+          // Dann war der letzte Eintrag am letzten Tag des jeweiligen Monats
+          if (today.getDate() !== 1) {
+            // Dann ist die neue Eingabe nicht an einem ersten Tag im Monat
+            return false;
+          } else if (old[1] === 0) {
+            // Dann war die letzte Eingabe am 31. Januar desselben Jahres.
+            if (today.getMonth() === 2) {
+              // Dann ist die neue Eingabe am 01. Februar und damit einen Tag später.
+              return true;
+            }
+          } else if (old[1] === 2) {
+            // Dann war die letzte Eingabe am 31. März desselben Jahres.
+            if (today.getMonth() === 3) {
+              // Dann ist die neue Eingabe am 01. April und damit einen Tag später.
+              return true;
+            }
+          } else if (old[1] === 4) {
+            // Dann war die letzte Eingabe am 31. Mai desselben Jahres.
+            if (today.getMonth() === 5) {
+              // Dann ist die neue Eingabe am 01. Juni und damit einen Tag später.
+              return true;
+            }
+          } else if (old[1] === 6) {
+            // Dann war die letzte Eingabe am 31. Juli desselben Jahres.
+            if (today.getMonth() === 7) {
+              // Dann ist die neue Eingabe am 01. August und damit einen Tag später.
+              return true;
+            }
+          } else if (old[1] === 7) {
+            // Dann war die letzte Eingabe am 31. August desselben Jahres.
+            if (today.getMonth() === 8) {
+              // Dann ist die neue Eingabe am 01. September und damit einen Tag später.
+              return true;
+            }
+          } else if (old[1] === 9) {
+            // Dann war die letzte Eingabe am 31. Oktober desselben Jahres.
+            if (today.getMonth() === 10) {
+              // Dann ist die neue Eingabe am 01. November und damit einen Tag später.
+              return true;
+            }
+          }
+        }
+      } else if (old[1] === 1) {
+        // Dann war der letzte Eintrag im Februar
+
+        if (today.getFullYear() % 4 === 0) {
+          // Dann handelt es sich um ein Schaltjahr.
+          if (
+            old[0] === 29 &&
+            today.getDate() === 1 &&
+            today.getMonth() === 2
+          ) {
+            // Dann war die letzte Eingabe am 29. Februar und die neue Eingabe ist am 01. März und damit einen Tag später.
+            return true;
+          }
+        } else {
+          // Dann handelt es sich um kein Schaltjahr.
+          if (
+            old[0] === 28 &&
+            today.getDate() === 1 &&
+            today.getMonth() === 2
+          ) {
+            // Dann war die letzte Eingabe am 28. Februar und die neue Eingabe ist am 01. März und damit einen Tag später.
+            return true;
+          }
+        }
+
+        if (today.getDate() === 1 && today.getMonth() === 2) {
+          // Dann ist die neue Eingabe am 01. März und damit einen Tag später.
+          return true;
+        }
+      }
+    } else if (old[2] === today.getFullYear() - 1) {
+      // Dann war die letzte Eingabe im letzten Jahr
+      if (
+        old[1] === 11 &&
+        old[0] === 31 &&
+        today.getMonth === 0 &&
+        today.getDate() === 1
+      ) {
+        // Dann war die letzte Eingabe am 31. Dezember des letzten Jahres und die neue Eingabe am 01. Januar des neuen Jahres und damit einen Tag später.
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+
   renderOverlay() {
     return;
   }
@@ -79,7 +227,7 @@ class Formular extends React.Component {
   renderDate() {
     const today = new Date();
 
-    return today.getFullYear() + "-" + today.getMonth + "-" + today.getDay;
+    return today.getFullYear() + "-" + today.getMonth() + "-" + today.getDay();
   }
 
   onSubmit = (formValues) => {
@@ -90,6 +238,7 @@ class Formular extends React.Component {
         "eintraege",
         JSON.stringify({
           entries: [{ realEintrag: formValues }],
+          streak: [1, this.renderCurrentDaySmall()],
         })
       );
       localStorage.setItem(
@@ -104,6 +253,17 @@ class Formular extends React.Component {
       const id = "realEintrag"; // `eintrag_${tmpEntries.entries.length + 1}`
 
       tmpEntries.entries.push({ [id]: formValues });
+      let streak = [tmpEntries.streak[0], tmpEntries.streak[1]];
+
+      const isStreak = this.checkStreak(streak[1]);
+
+      if (isStreak === true) {
+        streak[0] += 1;
+      } else if (isStreak === false) {
+        streak[0] = 0;
+      }
+
+      streak[1] = this.renderCurrentDaySmall();
 
       localStorage.removeItem("eintraege");
 
@@ -113,6 +273,7 @@ class Formular extends React.Component {
         "eintraege",
         JSON.stringify({
           entries,
+          streak,
         })
       );
     }
---
+++ app/src/utils.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+// Refactoring durchführen
